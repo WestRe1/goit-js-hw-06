f790a3b0b9d39f1d7c295121a95956a987638e5f
Jura Mazar <ura2003mazar@gmail.com>
2023-03-27 11:44:41 +0300

    .

--- js/task-08.js
+++ js/task-08.js
@@ -1,6 +1,18 @@
 const form = document.querySelector('.login-form');
-form.addEventListener('submit' onForm);
+form.addEventListener('submit', onForm);
 
 function onForm(evt) {
+    evt.preventDefault();
     
-}
+        const { email, password } = evt.currentTarget.elements;
+    if (email.value.length === 0 || password.value.length === 0) { 
+        alert('Заповніть незаповнене поле');
+    } else {
+        const value = {
+            email: email.value,
+            password: password.value,
+        }
+        console.log(value);
+        form.reset();
+    };
+};
