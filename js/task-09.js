a3016a10bda7382f53a67110cbd400cf9068f07d
Jura Mazar <ura2003mazar@gmail.com>
2023-03-27 12:00:30 +0300

    .

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const bkColor = document.querySelector('.change-color');
+const textColor = document.querySelector('.color')
+const body = document.querySelector('body')
+
+bkColor.addEventListener('click', onColor)
+
+function onColor(evt) {
+  body.style.backgroundColor = getRandomHexColor();
+  textColor.textContent = getRandomHexColor();
+};
